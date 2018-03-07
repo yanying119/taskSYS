a0baf53206ba1400ac9d5e0db8682706da20d1ef
刘武松 <liuws@chingo.cn>
2018-03-07 14:32:04 +0800

    添加日历空间

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,260 +1,259 @@
 <template>
-	<div id="content">
-		<div class="container">
-			<div class="task_list_box">
-				<ul class="task_list list-group">
-					<li class="list-group-item clearfix" v-for="(task,i) in taskList" @click="editor(i)">
-						<span>{{i+1}}、{{task.name}}</span>
-						<span class="glyphicon glyphicon-calendar rili fr" style="top:0">{{task.starTime}}至{{task.endTime}}</span><br>
-						<span v-if="task.isImportant">重要程度：<b class="red">加急</b></span>
-						<span v-else="task.isImportant">重要程度：不急</span><br>
-						<span v-if="task.isClose"><b class="green">完成与否：完成了</b></span></span>
-						<span v-else="task.isClose">完成与否：努力中</span><br>
-						<span v-html="task.content"></span>
-						<span  class="glyphicon glyphicon-remove-sign remove" @click="removeTask($event,i)"></span>
-					</li>
-					<li class="list-group-item">
-						<button class="btn btn-default" @click="newTask">新建任务</button>
-					</li>
-				</ul>
-			</div>
-			<div id="dialog" v-if="taskItem">
-				<div class="box">
-					<ul class="task_list list-group">
-						<li class="list-group-item">
-							任务名称：<input type="text" name="taskName" :value="taskItem.name">
-						</li>
-						<li class="list-group-item">
-							开始时间：<input type="text" name="taskStarTime" :value="taskItem.starTime"><span class="glyphicon glyphicon-calendar rili" style="top:0"></span>
-						</li>
-						<li class="list-group-item">
-							结束时间：<input type="text" name="taskEndTime"  :value="taskItem.endTime" ><span class="glyphicon glyphicon-calendar rili" style="top:0"></span>
-						</li>
+    <div id="content">
+        <div class="container">
+            <div class="task_list_box">
+                <ul class="task_list list-group">
+                    <li class="list-group-item clearfix" v-for="(task,i) in taskList" @click="editTash(i)" :key="'task'+i">
+                        <span>{{i+1}}、{{task.name}}</span><br>
+                        <span>开始时间：{{task.starTime}}</span><br>
+                        <span>结束时间：{{task.endTime}}</span><br>
+                        <span v-if="task.isImportant==1">重要程度：
+                            <b class="red">加急</b>
+                        </span>
+                        <span v-else>重要程度：不急</span><br>
+                        <span v-if="task.isClose">
+                            完成与否：<b class="green">完成了</b>
+                        </span>
+                        <span v-else>完成与否：努力中</span><br>
+                        <b>任务详情：</b><br>
+                        <span v-html="task.content"></span>
+                        <span class="glyphicon glyphicon-remove-sign remove" @click="removeTask($event,i)"></span>
+                    </li>
+                    <li class="list-group-item">
+                        <button class="btn btn-default" @click="newTask">新建任务</button>
+                    </li>
+                </ul>
+            </div>
+            <div id="dialog" v-show="showDialog">
+                <div class="box">
+                    <ul class="task_list list-group">
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">任务名称：</label>
+                                <input type="text" class="form-control" name="taskName" v-model="taskItem.name">
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">
+                                    开始时间：
+                                </label>
+                                <vue2datepicker v-model="taskItem.starTime" format="YYYY-MM-DD HH:mm:ss" type="datetime" />
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">
+                                    结束时间：
+                                </label>
+                                <vue2datepicker v-model="taskItem.endTime" format="YYYY-MM-DD HH:mm:ss" type="datetime" />
+                            </div>
+                        </li>
 
-						<li class="list-group-item" v-if="taskItem.isImportant">
-							重要程度：
-							<label for=""><input type="radio" name="import" checked="checked" value="1">加急</label>
-							<label for=""><input type="radio" name="import" value="0">不急</label>
-						</li>
-						<li class="list-group-item" v-else="taskItem.isImportant">
-							重要程度：
-							<label for=""><input type="radio" name="import"  value="1">加急</label>
-							<label for=""><input type="radio" name="import"  checked="checked" value="0">不急</label>
-						</li>
-
-						<li class="list-group-item" v-if="taskItem.isClose">
-							是否完成：
-							<label for=""><input type="radio" name="finsh"  value="0">努力中</label>
-							<label for=""><input type="radio" name="finsh" checked="checked" value="1">完成了</label>
-						</li>
-						<li class="list-group-item" v-else="taskItem.isClose">
-							是否完成：
-							<label for=""><input type="radio" name="finsh" checked="checked"  value="0">努力中</label>
-							<label for=""><input type="radio" name="finsh" value="1">完成了</label>
-						</li>
-
-						<li class="list-group-item">
-							<span class="fl">任务内容：</span><textarea name="content"  :value="taskItem.content"></textarea>
-						</li>
-						<li class="list-group-item">
-							<button class="btn btn-default" @click="saveTask">保存</button>
-							<button class="btn btn-default" @click="closeWindow">取消</button>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="wrap" v-show="taskItem"></div>
-	</div>
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">重要程度：</label>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="import" v-model="taskItem.isImportant"  :value="1"/>加急
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="import" v-model="taskItem.isImportant"  :value="0"/>不急
+                                    </label>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">是否完成：</label>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="finsh" :value="0" v-model="taskItem.isClose">努力中
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="finsh" :value="1" v-model="taskItem.isClose">完成了
+                                    </label>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="form-group">
+                                <label class="control-label">任务内容：</label>
+                                <textarea name="content" class="form-control" v-model="taskItem.content"></textarea>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <button class="btn btn-default" @click="saveTask">保存</button>
+                            <button class="btn btn-default" @click="closeDialog">取消</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="wrap" v-show="showDialog"></div>
+    </div>
 </template>
+<script>
+import vue2datepicker from 'vue2-datepicker';
+import moment from 'moment'
+export default {
+    data: function () {
+        return {
+            taskList: [],
+            taskItem: {},
+            currentCount: 0,
+            showDialog:false,
+        }
+    },
+    components: { vue2datepicker },
+    methods: {
+        newTask: function () {
+            var _this = this;
+            this.showDialog = true;
+            this.taskItem = {};
+            this.currentCount = this.taskList.length;
+            setTimeout(function () {
+                _this.dialogPosition()
+            })
+        },
+        editTash: function (i) {
+            var _this = this;
+            this.showDialog = true;
+            this.taskItem = this.taskList[i]
+            this.currentCount = i;
+            setTimeout(function () {
+                _this.dialogPosition()
+            })
+        },
+        saveTask: function () {
+            var dialog = document.getElementById("dialog"),
+                i = this.currentCount;
+            this.taskList[i] = Object.assign({},this.taskItem,{
+                starTime:moment(this.taskItem.starTime).format("YYYY-MM-DD HH:mm:ss"),
+                endTime:moment(this.taskItem.endTime).format("YYYY-MM-DD HH:mm:ss")
+            });
+            this.taskItem = {};
+            this.showDialog = false;
+            this.saveData("taskList", this.taskList);
+        },
+        saveData: function (name, content) {
+            if (window.localStorage) {
+                if (typeof content == "string") {
+                    localStorage[name] = content;
+                } else if (typeof content == "object") {
+                    localStorage[name] = JSON.stringify(this.taskList);
+                } else {
+                    localStorage[name] = String(this.taskList);
+                }
+            }
+        },
+        removeTask: function (e, i) {
+            if (confirm("您真的要删掉吗，删掉就没了")) {
+                e.stopPropagation();
+                this.taskList.splice(i, 1);
+                this.saveData("taskList", this.taskList);
+            } else {
+                e.stopPropagation();
+            }
+        },
+        dialogPosition: function () {
+            var dialog = document.getElementById("dialog"),
+                height = document.documentElement.clientHeight;
+            var dialogHeight = dialog.clientHeight,
+                top = (height - dialogHeight) / 2;
+
+            if (top > 0) {
+                dialog.style.top = top + "px";
+            } else {
+                dialog.style.height = (height - 136) + "px";
+                dialog.style.top = 78 + "px";
+            }
+        },
+        closeDialog: function () {
+            this.showDialog = false;
+        },
+        getData: function (name) {
+            if (window.localStorage) {
+                return localStorage.getItem(name);
+            }
+        },
+        init: function () {
+            if (this.getData("taskList")) {
+                this.taskList = JSON.parse(this.getData("taskList"));
+            }
+        }
+    },
+    mounted: function () {
+        this.init();
+    }
+}
+</script>
 
 <style lang="scss">
-	#content {
-		height: 100%;
-		overflow-y: auto;
-		padding:66px 0 50px;
-	}
-	.task_list_box {
-		padding:10px 0;
-	}
-	.fl {
-		float: left;
-	}
-	.fr {
-		float: right;
-	}
-	.rili {
-		color:#4399d8;
-	}
-	.red {
-		color:#f00;
-	}
-	.list-group-item {
-		position: relative;
-		span {
-			line-height: 30px;
-		}
-		label {
-			font-weight: normal;
-		}
-		.remove {
-			position: absolute;
-			right:-5px;
-			top: -4px;
-			line-height: 15px;
-		}
-	}
-	.green {
-		color:#1f8214;
-		font-weight: normal;
-	} 
-	#dialog {
-		position: fixed;
-		left: 5%;
-		width: 90%;
-		z-index: 10;
-		max-width: 600px;
-		overflow-y:auto;
-		border-radius:4px;
-		background: #fff;
-	}
-	.box {
-		overflow: hidden;
-	}
-	.wrap {
-		position: fixed;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		z-index: 9;
-		background: #000;
-		opacity: 0.5;
-	}
-	.task_list {
-		margin-bottom:0;
-	}
-	input,textarea {
-		width: 100%;
-		max-width: 154px;
-	}
+#content {
+  height: 100%;
+  overflow-y: auto;
+  padding: 66px 0 50px;
+}
+.task_list_box {
+  padding: 10px 0;
+}
+.fl {
+  float: left;
+}
+.fr {
+  float: right;
+}
+.rili {
+  color: #4399d8;
+}
+.red {
+  color: #f00;
+}
+.list-group-item {
+  position: relative;
+  span {
+    line-height: 30px;
+  }
+  label {
+    font-weight: normal;
+  }
+  .remove {
+    position: absolute;
+    right: -5px;
+    top: -4px;
+    line-height: 15px;
+  }
+}
+.green {
+  color: #1f8214;
+  font-weight: normal;
+}
+#dialog {
+  position: fixed;
+  left: 5%;
+  width: 90%;
+  z-index: 10;
+  overflow-y: auto;
+}
+.box {
+  overflow: hidden;
+  max-width: 600px;
+  background: #fff;
+  margin: 0 auto;
+  border-radius: 4px;
+}
+.wrap {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9;
+  background: #000;
+  opacity: 0.5;
+}
+.task_list {
+  margin-bottom: 0;
+}
 </style>
-
-<script>
-	export default{
-		data:function(){
-	        return {
-	            taskList:[],
-	            taskItem:null,
-	            currentCount:0
-	        }
-	    },
-	    methods:{
-	    	editor:function(i){
-	    		var _this= this;
-	    		this.taskItem = this.taskList[i]
-	    		this.currentCount = i;
-
-	    		setTimeout(function(){
-	    			_this.dialogPosition()
-	    		})
-
-	    	},
-	    	removeTask:function(e,i){
-	    		if(confirm("您真的要删掉吗，删掉就没了")){
-	    			e.stopPropagation();
-	    			this.taskList.splice(i,1);
-	    			this.saveData("taskList",this.taskList);
-	    		}else {
-	    			e.stopPropagation();
-	    		}
-	    	},
-	    	dialogPosition:function(){
-	    		var dialog = document.getElementById("dialog"),
-		    			height = document.documentElement.clientHeight;
-
-		    		var	dialogHeight = dialog.clientHeight,
-		    			top = (height - dialogHeight)/2;
-
-		    			if (top > 0 ){
-		    				dialog.style.top = top +"px";
-		    			}else {
-		    				dialog.style.height = (height - 136)+"px";
-		    				dialog.style.top = 78 +"px";
-		    				console.log(dialog.style.height)
-		    			}
-	    	},
-	    	saveTask:function(){
-	    		var dialog = document.getElementById("dialog"),
-	    			name = document.getElementsByName("taskName")[0].value,
-                    starTime = document.getElementsByName("taskStarTime")[0].value,
-                    endTime = document.getElementsByName("taskEndTime")[0].value,
-                    content = document.getElementsByName("content")[0].value,
-                    isClose = this.getRadio(document.getElementsByName("finsh")),
-                    isImportant = this.getRadio(document.getElementsByName("import")),
-                    i = this.currentCount;
-                var newTask = {
-                	name:name,
-                    starTime:starTime,
-                    endTime:endTime,
-                    content:content,
-                    isClose:isClose,
-                    isImportant:isImportant
-                }
-
-                this.taskList[i] = newTask;
-                this.taskItem = null;
-                this.saveData("taskList",this.taskList);
-	    	},
-	    	getRadio:function(obj){
-
-	    		for(var i = 0; i<obj.length;i++){
-	    			if(obj[i].checked){
-	    				if(obj[i].value == 1){
-	    					return true;
-	    				}else{
-	    					return false;
-	    				}
-	    			}
-	    		}
-	    	},
-	    	saveData:function(name,content){
-
-	    		if(window.localStorage){
-	    			if(typeof content == "string"){
-	    				localStorage[name] = content;
-	    			}else if(typeof content == "object"){
-	    				localStorage[name] = JSON.stringify(this.taskList);
-	    			}else{
-	    				localStorage[name] = String(this.taskList);
-	    			}
-	    		}
-	    	},
-	    	getData:function(name){
-	    		if(window.localStorage){
-	    			return localStorage.getItem(name);
-	    		}
-	    	},
-	    	newTask:function(){
-	    		var _this = this;
-	    		this.taskItem = true;
-	    		this.currentCount = this.taskList.length;
-	    		setTimeout(function(){
-	    			_this.dialogPosition()
-	    		})
-	    	},
-	    	closeWindow:function(){
-	    		this.taskItem = false;
-	    	},
-	    	init:function(){
-	    		if(this.getData("taskList")){
-
-	    			this.taskList = JSON.parse(this.getData("taskList"));
-	    		}
-	    	}
-	    },
-	    mounted:function(){
-	    	this.init();
-	    }
-	}
-</script>
